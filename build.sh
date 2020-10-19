9d2873d0946690f58357c355069abd92e1e00926
Stefan Ivanovski <stefs.ivanovski@tuta.io>
2020-10-19 21:12:45 +0200

    Linux build

--- build.sh
+++ build.sh
@@ -1,18 +1,12 @@
-#!/bin/bash
-
-cppflags='-O2 -Wall -Wextra -fno-exceptions'
+cppflags='-O2 -Wall -Wextra'
 
 if [ ! -f glad.o ]; then
     clang $cppflags -c thirdparty/glad/src/glad.c -I thirdparty/glad/include
 fi
 
-if [ ! -f stb_image.o ]; then
-    clang $cppflags -c thirdparty/stb_image/stb_image.c -I thirdparty/stb_image
-fi
-
 conf=$(sdl2-config --cflags --libs)
-tp_includes='-I thirdparty/glad/include -I thirdparty/stb_image'
+tp_includes='-I thirdparty/glad/include -I thirdparty/glm'
 tp_libs='-ldl'
-source='src/*.cpp src/window/*.cpp src/shader/*.cpp src/texture/*.cpp src/asset_load/*.cpp'
+source='src/*.cpp src/window/*.cpp src/shader/*.cpp src/texture/*.cpp src/asset_load/*.cpp src/event/*.cpp'
 
-clang++ $cppflags $source glad.o stb_image.o -o editor $conf $tp_includes $tp_libs
+clang++ $cppflags $source glad.o -o editor $conf $tp_includes $tp_libs
