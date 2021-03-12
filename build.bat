b8aac2064018cc54918f36d14e25fcf4aaac7d8b
Stefan Ivanovski <stefs.ivanovski@tuta.io>
2021-03-12 18:47:18 +0100

    Move build files to separate folder

--- build.bat
+++ build.bat
@@ -7,8 +7,9 @@
 
 @echo off
 
-set "cppflags=/O2 /W4 /EHsc /D_CRT_SECURE_NO_WARNINGS"
+set "cppflags=/O2 /W4 /EHsc /D_CRT_SECURE_NO_WARNINGS /Fo:build/ /Fe:build/ /Fd:build/"
 set "tp_includes=-I ./thirdparty/glad/include -I ./thirdparty/SDL2/include -I ./thirdparty/glm -I ./thirdparty/freetype/include"
 
-cl %cppflags% -c thirdparty/glad/src/glad.c -I thirdparty/glad/include
-cl %cppflags% .\glad.obj .\src\main.cpp .\src\asset_load\*.cpp .\src\shader\*.cpp .\src\texture\*.cpp .\src\window\*.cpp .\src\event\*.cpp .\src\editor\*.cpp %tp_includes% /link .\thirdparty\freetype\lib\freetype.lib .\thirdparty\SDL2\lib\SDL2.lib /out:editor.exe
+mkdir build
+cl %cppflags% -c ./thirdparty/glad/src/glad.c -I ./thirdparty/glad/include
+cl %cppflags% .\build\glad.obj .\src\main.cpp .\src\asset_load\*.cpp .\src\shader\*.cpp .\src\texture\*.cpp .\src\window\*.cpp .\src\event\*.cpp .\src\editor\*.cpp %tp_includes% /link .\thirdparty\freetype\lib\freetype.lib .\thirdparty\SDL2\lib\SDL2.lib /out:editor.exe
